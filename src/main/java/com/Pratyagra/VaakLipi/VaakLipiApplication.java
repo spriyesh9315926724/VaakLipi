1a560f814b0b0889fa683d0b813eb6767ad976b0
spriyesh9315926724 <156476572+spriyesh9315926724@users.noreply.github.com>
2024-01-18 21:25:39 +0530

    Amending phase 1 changes

---
+++ src/main/java/com/Pratyagra/VaakLipi/VaakLipiApplication.java
@@ -0,0 +1,13 @@
+package com.Pratyagra.VaakLipi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaakLipiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaakLipiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pratyagra/VaakLipi/VaakLipiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pratyagra.VaakLipi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaakLipiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
