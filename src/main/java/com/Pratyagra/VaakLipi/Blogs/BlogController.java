7e2ffc8b8f67ed4990316d88577752b1c3fe7843
spriyesh9315926724 <156476572+spriyesh9315926724@users.noreply.github.com>
2024-01-20 12:23:43 +0530

    Amending phase 1 changes

---
+++ src/main/java/com/Pratyagra/VaakLipi/Blogs/BlogController.java
@@ -0,0 +1,9 @@
+package com.Pratyagra.VaakLipi.Blogs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+}
---
+++ src/main/java/com/Pratyagra/VaakLipi/Comments/CommentController.java
@@ -0,0 +1,10 @@
+package com.Pratyagra.VaakLipi.Comments;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogs/{blogId}/comments")
+public class CommentController {
+}
